30c36967068fab4ab599fb03398d527bc49a95d7
Vlad-Rares Raducu <vlad.raducu@wisercat.net>
2024-06-29 20:16:02 +0300

    [blog-list][4.8] Solved exercise 4.8, step 1

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const config = require('./utils/config')
+require('express-async-errors')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 
 
@@ -20,6 +22,11 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+
+app.use(middleware.requestLogger)
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
