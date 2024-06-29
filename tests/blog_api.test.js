542dfc3231c1a520b06c35df1c665be3e91faf83
Vlad-Rares Raducu <vlad.raducu@wisercat.net>
2024-06-29 20:22:46 +0300

    [blog-list][4.9] Solved exercise 4.9, step 2

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -30,7 +30,7 @@ beforeEach(async () => {
   await blogObject.save()
 })
 
-test('blogs are returned as json', async () => {
+test.only('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
@@ -41,6 +41,13 @@ test.only('there are two notes', async () => {
   const response = await api.get('/api/blogs')
 
   assert.strictEqual(response.body.length, initialBlogs.length)
+})
+
+test.only('has unique property id', async () => {
+  const response = await api.get('/api/blogs')
+  const firstBlog = response.body[0]
+
+  assert.strictEqual(firstBlog.hasOwnProperty('id'), true)
 })
 
 after(async () => {
