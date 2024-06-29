7f3b2556adc1ab59fd4dc0255dd9afe1853d0dc0
Vlad-Rares Raducu <vlad.raducu@wisercat.net>
2024-06-29 14:59:18 +0300

    [blog-list] [4.4] Solved exercise 4.4, step 2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1;
 }
 
+const totalLikes =  (blogs) => {
+  if (blogs === 'undefined') {
+    return 0;
+  }
+  return blogs.length > 0
+    ? blogs.map(blog => blog.likes).reduce((acc, curr) => acc + curr, 0)
+    : 0;
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
