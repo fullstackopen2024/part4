a43fafe6450dc828af873bfbeaa92cb6c45f0b96
Vlad-Rares Raducu <vlad.raducu@wisercat.net>
2024-06-30 14:06:46 +0300

    [blog-list][4.15] Solved exercise 4.15, step 3

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,5 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-const { request, response } = require("express");
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
